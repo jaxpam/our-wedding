073b8a173a3e02a203c734637c0c2d3dd6ff5650
Ste Pammenter <spammenter@live.com>
2020-07-20 17:35:44 +0100

    Initial markup.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
+<main>
+<section>
+  <h2>Stephen & Jacquie</h2>
+  <img alt="Ste & Jax enjoy the sun in Berlin." src="//placehold.it/1020x1980">
+  <p>
+    Thanks for stopping by, we can't wait to celebrate our wedding day with you!
+  </p>
+</section>
+<section>
   <div>
-    Wedingdong
+    <img alt="Small white wedding flower." src="//placehold.it/100x300">
+  <h1>Save the date<a href="#footnote-*">*</a></h1>
+  <address>Conniston</address>
+  <time datetime="2021-05-29T13:00:00.000Z">Saturday, May 29th, 2012</time>
+   <button class="btn btn-primary" type="button">Add date to calendar</button>
+   <img alt="Large white wedding flower." src="//placehold.it/100x300">
   </div>
+  <footer id="footnote-*">* more details to come</footer>
+</section>
+</main>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
