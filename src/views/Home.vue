5e5a915849548f6b691f72eb1e1e32807a7e3f71
Ste Pammenter <spammenter@live.com>
2020-07-21 16:42:46 +0100

    Fonts, images and some styles.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <main>
     <b-container fluid>
-      <b-row no-gutters>
-        <b-col md="6" tag="section" class="section">
-          <b-row align-h="center">
+      <b-row>
+        <b-col md="6" tag="section" class="section section--dark">
+          <b-row>
             <b-col class="my-5">
               <h2>Stephen & Jacquie</h2>
               <img
                 class="section__background-img"
                 alt="Ste & Jax enjoy the sun in Berlin."
-                src="//placehold.it/1020x1980"
+                src="../assets/img/us.jpg"
                 sizes="(max-width: 376px) 100vw, (max-width: 576px) 50vw"
-                srcset="//placehold.it/376x376/0f0 376w, //placehold.it/576x500/00f 576w, //placehold.it/800x800/f00 800w"
+                srcset="../assets/img/us-375.jpg 375w, ../assets/img/us-960.jpg 960w, ../assets/img/us.jpg 1080w"
               />
               <p>Thanks for stopping by, we can't wait to celebrate our wedding day with you!</p>
             </b-col>
@@ -19,7 +19,7 @@
         </b-col>
         <b-col md="6" tag="section">
           <div>
-            <img alt="Small white wedding flower." src="//placehold.it/100x300" />
+            <img alt="Small white wedding flower." src="../assets/img/small-flower.svg" />
             <h1>
               Save the date
               <a href="#footnote-*">*</a>
