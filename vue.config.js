d9b430457eca3fe4ad4dc5212fd7aa12e18fe115
Ste Pammenter <spammenter@live.com>
2020-07-21 16:42:28 +0100

    Image optimization.

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,47 @@
 
 const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const ImageminPlugin = require("imagemin-webpack");
+const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 
 module.exports = {
     productionSourceMap: false,
+    // chainWebpack: config => {
+    //   const imgRule = config.module.rule("svg");
+    //   imgRule
+    //     .use("file-loader")
+    //     .loader("image-webpack-loader")
+    //     .tap(options => {
+    //       const ret = options || {};
+    //       ret.mozjpeg = {
+    //         progressive: true,
+    //         quality: 100
+    //       };
+    //       ret.webp = {
+    //         quality: 80
+    //       };
+
+    //       ret.svgo = {
+    //         removeMetadata: true,
+    //         removeComments: true
+    //       }
+    //       console.log(ret);
+    //       return ret;
+    //     });
+    // },
     configureWebpack: {
-      output: {
-        libraryExport: "default"
-      },
       plugins: [
         new PrerenderSPAPlugin({
             staticDir: path.join(__dirname, 'dist'),
             routes: [ '/' ],
-          })
+          }),
+          new ImageminPlugin({
+            cache: true,
+            imageminOptions: {
+              plugins: [["mozjpeg", {quality: 80}]]
+            },
+          }),
+          new ImageminWebpWebpackPlugin()
       ]
     },
   
