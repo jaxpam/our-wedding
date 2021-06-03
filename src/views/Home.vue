366cda0b6c8d943de54079332969ee3c4c4639b3
Jax Steele <jax@ph-creative.com>
2021-06-03 16:47:54 +0100

    Updates to content and favicon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
           class="img-fluid hero__img"
           alt="Ste and Jax hands holding on the day of their wedding"
           :style="{
-            backgroundImage: `url('${require('@/assets/img/Jax&Ste-56.jpg?placeholder')}')`
+            backgroundImage: `url('${require('@/assets/img/Jax&Ste-56.jpg?placeholder')}')`,
           }"
           :src="require('@/assets/img/Jax&Ste-56.jpg')"
           :srcset="require('@/assets/img/Jax&Ste-56.jpg?srcset&format=webp')"
@@ -23,21 +23,25 @@
             <div class="col-12 col-lg-6">
               <h1>Stephen & Jacquie's Wedding Reception</h1>
               <p>
-                2020 was a strange year for everyone and a stranger year to get
-                married! We're sorry we couldn't have you with us when when we
-                tied the knot, but we would still love celebrate our nuptials
-                with everyone.
+                2020 was an interesting year! Due to restrictions and the
+                uncertainty of our May 2021 wedding, we decided to tie the knot.
+              </p>
+              <p>
+                We're sorry we couldn't have you with us when we said 'I
+                Do', but we would still love to celebrate our nuptials with you.
               </p>
               <p>
                 As we postponed what was going to be our wedding and turn it
                 into a celebration a year later. We can't forsee what
-                restrictions will be like, but we hope, all being well, you can
-                join us for a party in The Lakes on 28th May 2022.
+                restrictions will be like in the next year, but we hope all
+                being well, you can join us for a party in The Lakes on 28th May
+                2022.
               </p>
               <p>
-                Given our professions we've opted to send invites digitally. You
-                can register your email below, and we'll provide an RSVP with
-                more details nearer to the time.
+                Given our professions, and that we're trying to be a little more
+                eco-friendly, we've opted to send invites digitally. You can
+                register your email below, and we'll provide more details, along
+                with an RSVP, nearer to the time.
               </p>
               <iframe
                 src="https://cdn.forms-content.sg-form.com/d313292d-b992-11eb-991d-bed3631b86b2"
@@ -51,7 +55,7 @@
                   <time class="h3" datetime="2021-05-29T13:00:00.000Z"
                     >Saturday, May 28th, 2022</time
                   >
-                  <address class="h5">Conniston, The Lakes</address>
+                  <address class="h5">Coniston, The Lakes</address>
                 </div>
               </div>
               <picture class="img-fluid">
